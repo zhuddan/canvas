635d0c09335dcf599d859b901b0ec54e67357cff
zhuddan <yobaci@163.com>
2024-08-28 17:47:08 +0800

    fix

--- dist/index.js
+++
@@ -1,2 +0,0 @@
-export { App, Picture, Shape, Text, TextStyle } from './canvas.js';
-//# sourceMappingURL=index.js.map
--- rollup.config.build.mjs
+++ rollup.config.build.mjs
@@ -1,187 +1,69 @@
 // @ts-check
 import process from 'node:process'
-import fs from 'node:fs'
-import path from 'node:path'
 import { defineConfig } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
 import del from 'rollup-plugin-delete'
-import serve from 'rollup-plugin-serve'
-import livereload from 'rollup-plugin-livereload'
 import terser from '@rollup/plugin-terser'
 import { dts } from 'rollup-plugin-dts'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { exec } from './scripts/exec.mjs'
 
-// const repo = JSON.parse(readFileSync('./package.json').toString())
-const isProduction = process.env.NODE_ENV === 'production'
-
-/**
- *
- * @param {string} root
- * @param {string} extname
- * @param {string} append
- */
-export function createInput(root, extname, append = '') {
-  const files = fs.readdirSync(root)
-  /**
-   * @type {{ [entryAlias: string]: string }}
-   */
-  let input = {}
-  for (let index = 0; index < files.length; index++) {
-    const maybeFile = files[index]
-    const filePath = path.join(root, maybeFile)
-    const stat = fs.lstatSync(filePath)
-    if (stat.isDirectory()) {
-      input = {
-        ...input,
-        ...createInput(filePath, extname, append),
-      }
-    }
-    else if (stat.isFile()) {
-      if (filePath.endsWith(extname)) {
-        const root = `${filePath.split('\\').shift()}\\`
-        const inputFile = filePath.replace(root, '').replace(extname, '')
-        input[inputFile + append] = filePath
-      }
-    }
-  }
-  return input
-}
-
 function main() {
-  /**
-   * @type {import('rollup').RollupOptions[]}
-   */
-  const result = []
-  /**
-   * @type {import('rollup').InputOption}
-   */
-  const inputTs = createInput('src', '.ts', isProduction ? '' : '')
-  if (!isProduction) {
-    for (const key in inputTs) {
-      if (Object.hasOwnProperty.call(inputTs, key)) {
-        const element = inputTs[key]
-        console.log(key)
-        console.log(element)
-      }
-    }
-  }
-  /**
-   * @type {import('rollup').OutputOptions[]}
-   */
-  const outputJs = [{
-    dir: 'dist',
-    format: 'esm',
-    entryFileNames: '[name].mjs',
-  }]
-
-  /**
-   *  @type {import('rollup').InputOptions['plugins']}
-   */
-  const commonPlugins = [
-    isProduction
-      ? del({
-        targets: ['dist/*', 'temp/*'],
-        force: true,
-        hook: 'buildStart',
-      })
-      : del({
-        targets: ['dist/*', 'temp/*'],
-        force: true,
-        hook: 'buildStart',
-      }),
-    typescript(isProduction
-      ? {
-          declaration: false,
-        }
-      : {
-
+  return defineConfig([
+    {
+      input: './src/index.ts',
+      output: [
+        {
+          format: 'es',
+          dir: 'dist',
+          entryFileNames: '[name].esm.js',
+          sourcemap: true,
+        },
+        {
+          format: 'commonjs',
+          dir: 'dist',
+          entryFileNames: '[name].cjs.js',
+          sourcemap: true,
+        },
+      ],
+      plugins: [
+        del({
+          targets: ['dist/*', 'temp/*'],
+          force: true,
+          hook: 'buildStart',
         }),
-    nodeResolve(),
-    commonjs(),
-  ]
-
-  if (isProduction) {
-    commonPlugins.push(terser())
-    outputJs.push({
-      dir: 'dist',
-      format: 'cjs',
-      entryFileNames: '[name].js',
-    })
-    outputJs.forEach((e) => {
-      e.sourcemap = true
-    })
-  }
-  else {
-    commonPlugins.push(
-      livereload(),
-      serve({
-        port: 13000,
-        contentBase: '.',
-        openPage: '/example/index.html',
-        open: true,
-      }),
-    )
-  }
-
-  result.push({
-    input: inputTs,
-    output: outputJs,
-    plugins: commonPlugins,
-  })
-
-  /**
-   * 打包类型声明
-   */
-  if (isProduction) {
-    /**
-     * @type {import('rollup').InputOption}
-     */
-    const inputDTs = createInput('temp', '.d.ts')
-    /**
-     * @type {import('rollup').OutputOptions[]}
-     */
-    const outputDts = [
-      {
-        dir: 'dist',
+        typescript({ declaration: false }),
+        nodeResolve(),
+        commonjs(),
+        terser(),
+      ],
+    },
+    {
+      input: './temp/index.d.ts',
+      output: {
+        file: 'dist/index.d.ts',
         format: 'es',
       },
-    ]
-    /**
-     *  @type {import('rollup').InputOptions['plugins']}
-     */
-    const plugins = [
-      nodeResolve(),
-      commonjs(),
-      dts({
-        respectExternal: true,
-      }),
-      {
-        name: 'before',
-        buildStart: async () => {
-          const { ok, stderr } = await exec('tsc', ['-p', 'tsconfig.build.json'])
-          if (!ok) {
-            console.error('TypeScript compilation failed:', stderr)
-            process.exit(1)
-          }
+      plugins: [
+        nodeResolve(),
+        commonjs(),
+        dts({
+          respectExternal: true,
+        }),
+        {
+          name: 'before',
+          buildStart: async () => {
+            const { ok, stderr } = await exec('tsc', ['-p', 'tsconfig.build.json'])
+            if (!ok) {
+              console.error('TypeScript compilation failed:', stderr)
+              process.exit(1)
+            }
+          },
         },
-      },
-    ]
-    result.push({
-      input: inputDTs,
-      output: outputDts,
-      plugins,
-      // external: [
-      //   ...external,
-      //   // 'csstype',
-      //   // convertPackageNameToRegExp('csstype'),
-      // ],
-    })
-  }
-
-  console.log(result.length)
-  return defineConfig(result)
+      ],
+    },
+  ])
 }
 
 export default main()
