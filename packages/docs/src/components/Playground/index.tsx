a6fd8f143d286553bb693ef3ef0c7af4be86a292
zhuddan <yobaci@163.com>
2024-09-27 21:05:22 +0800

    1

--- packages/docs/src/components/Playground/index.tsx
+++ packages/docs/src/components/Playground/index.tsx
@@ -1,11 +1,14 @@
-import React, { Children, useEffect, useMemo, useState } from 'react'
+import React, { Children, useCallback, useEffect, useMemo, useState } from 'react'
 import { Sandpack, SandpackPreview, SandpackProvider } from '@codesandbox/sandpack-react'
 import type { Monaco } from '@monaco-editor/react'
 import Editor, { loader } from '@monaco-editor/react'
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import dts from '!!raw-loader!@zd~/canvas/dist/index.d.ts'
+import devDts from '!!raw-loader!@zd~/canvas/dist/index.d.ts'
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import js from '!!raw-loader!@zd~/canvas/dist/index.esm.js'
 
+const debug = false
 const customHtml = `
 <!DOCTYPE html>
 <html lang="en">
@@ -23,18 +26,23 @@ const customHtml = `
       }
     </style>
   </head>
-  <body style="background:red">
+  <body >
     <script src="./index.ts" type="module"></script>
   </body>
 </html>
   `
 
-function Playground({ children }: React.PropsWithChildren) {
+function Playground({
+  children,
+  tips = false,
+}: React.PropsWithChildren<{
+  tips?: boolean | string
+}>) {
   const handleEditorWillMount = (monaco: Monaco) => {
     monaco.languages.typescript.typescriptDefaults.addExtraLib(
       `
-      declare module '@zd~/canvas' {
-        ${dts}
+      declare module '${debug ? './canvas' : '@zd~/canvas'}' {
+        ${devDts}
       }
       `,
     )
@@ -71,23 +79,7 @@ function Playground({ children }: React.PropsWithChildren) {
 
   return (
     <>
-      <div className="bg-blue-50 border-l-4 border-blue-500 p-4 mb-4 rounded shadow-md">
-        <div className="flex items-center space-x-2 mb-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 256 256"
-            className="text-blue-500"
-          >
-            <path fill="currentColor" d="M240 128a15.74 15.74 0 0 1-7.6 13.51L88.32 229.65a16 16 0 0 1-16.2.3A15.86 15.86 0 0 1 64 216.13V39.87a15.86 15.86 0 0 1 8.12-13.82a16 16 0 0 1 16.2.3l144.08 88.14A15.74 15.74 0 0 1 240 128" />
-          </svg>
-          <h3 className="text-lg font-semibold text-blue-700 mb-0">动手实践</h3>
-        </div>
-        <div className="text-blue-600">
-          请尝试修改 Playground 中的代码，查看变化
-        </div>
-      </div>
+      {tips && <Tips />}
       <div
         className="flex h-[350px] border border-[#ccc] border-solid  my-4"
       >
@@ -111,8 +103,15 @@ function Playground({ children }: React.PropsWithChildren) {
             style={{ height: '100%' }}
             template="vanilla-ts"
             files={{
-              '/index.ts': code,
               '/index.html': customHtml,
+              ...(
+                debug
+                  ? {
+                      '/canvas.ts': js,
+                    }
+                  : undefined
+              ),
+              '/index.ts': code,
             }}
             customSetup={{
               entry: '/index.html',
@@ -134,6 +133,32 @@ function Playground({ children }: React.PropsWithChildren) {
         </div>
       </div>
     </>
+  )
+}
+
+function Tips({
+  msg = '请尝试修改 Playground 中的代码，查看变化',
+}: {
+  msg?: string
+}) {
+  return (
+    <div className="bg-blue-50 border-l-4 border-blue-500 p-4 mb-4 rounded shadow-md">
+      <div className="flex items-center space-x-2 mb-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 256 256"
+          className="text-blue-500"
+        >
+          <path fill="currentColor" d="M240 128a15.74 15.74 0 0 1-7.6 13.51L88.32 229.65a16 16 0 0 1-16.2.3A15.86 15.86 0 0 1 64 216.13V39.87a15.86 15.86 0 0 1 8.12-13.82a16 16 0 0 1 16.2.3l144.08 88.14A15.74 15.74 0 0 1 240 128" />
+        </svg>
+        <h3 className="text-lg font-semibold text-blue-700 mb-0">动手实践</h3>
+      </div>
+      <div className="text-blue-600">
+        {msg}
+      </div>
+    </div>
   )
 }
 
